8502fbc214fefa2e66e47a55d42a945e77625c08
xereda <jacksonrs@gmail.com>
2017-04-27 10:26:18 -0300

    Formulario pronto.

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,32 +1,45 @@
 <template>
   <div class="">
-    {{ formFields }}
+    <pre>{{ formFields }}</pre>
+    <pre>{{ $v.formFields }}</pre>
     <div class="columns is-multiline">
       <div class="column is-4">
         <div class="field">
           <label class="label">Nome *</label>
           <p class="control">
-            <input :class="{ 'input': true, 'is-danger': true }" type="text" placeholder="Informe seu nome completo" v-model="formFields.name">
+            <input :class="{ 'input': true, 'is-danger': $v.formFields.name.$error, 'is-success': !$v.formFields.name.$error && $v.formFields.name.$dirty }"
+                   type="text"
+                   placeholder="Informe seu nome completo"
+                   @input="$v.formFields.name.$touch()"
+                   v-model="formFields.name">
           </p>
-          <p class="help is-danger">Informe corretamente seu nome.</p>
+          <p class="help is-danger" v-show="$v.formFields.name.$error">Informe corretamente seu nome.</p>
         </div>
       </div>
       <div class="column is-3">
         <div class="field">
           <label class="label">E-mail *</label>
           <p class="control">
-            <input :class="{ 'input': true, 'is-danger': true }" type="email" placeholder="Informe seu e-mail" v-model="formFields.email">
+            <input :class="{ 'input': true, 'is-danger': $v.formFields.email.$error, 'is-success': !$v.formFields.email.$error && $v.formFields.email.$dirty }"
+                   type="email"
+                   @input="$v.formFields.email.$touch()"
+                   placeholder="Informe seu e-mail"
+                   v-model="formFields.email">
           </p>
-          <p class="help is-danger">Informe corretamente seu e-mail.</p>
+          <p class="help is-danger" v-show="$v.formFields.email.$error">Informe corretamente seu e-mail.</p>
         </div>
       </div>
       <div class="column is-3">
         <div class="field">
           <label class="label">Telefone *</label>
           <p class="control">
-            <input :class="{ 'input': true, 'is-danger': true }" type="text" placeholder="(11) 99876-5432" v-model="formFields.phone">
+            <input :class="{ 'input': true, 'is-danger': $v.formFields.phone.$error, 'is-success': !$v.formFields.phone.$error && $v.formFields.phone.$dirty }"
+                   type="text"
+                   @input="$v.formFields.phone.$touch()"
+                   placeholder="(11) 99876-5432"
+                   v-model="formFields.phone">
           </p>
-          <p class="help is-danger">Informe um telefone de contato.</p>
+          <p class="help is-danger" v-show="$v.formFields.phone.$error">Informe um número válido.</p>
         </div>
       </div>
       <div class="column">
@@ -50,22 +63,29 @@
               <label class="label">Como soube da JMOB? *</label>
               <p class="control">
                 <span class="select">
-                  <select v-model="formFields.hearAboutUs" @change="setHearAboutUsDetails()">
+                  <select v-model="formFields.hearAboutUs"
+                          @click="$v.formFields.hearAboutUs.$touch()"
+                          :class="{ 'is-danger-select': $v.formFields.hearAboutUs.$error, 'is-success-select': !$v.formFields.hearAboutUs.$error && $v.formFields.hearAboutUs.$dirty }"
+                          @change="setHearAboutUsDetails()">
                     <option value="">Selecione uma opção</option>
                     <option :value="option" v-for="option in selectOptions">{{ option }}</option>
                   </select>
                 </span>
               </p>
-              <p class="help is-danger">Selecione uma das opções.</p>
+              <p class="help is-danger" v-show="$v.formFields.hearAboutUs.$error">Selecione uma das opções.</p>
             </div>
           </div>
           <div class="column" v-show="formFields.hearAboutUs === 'Indicação' || formFields.hearAboutUs === 'Outro'">
             <div class="field">
               <label class="label">{{ hearAboutUs.label }}</label>
               <p class="control">
-                <input :class="{ 'input': true, 'is-danger': true }" type="text" :placeholder="hearAboutUs.placeholder" v-model="formFields.hearAboutUsDetail">
+                <input :class="{ 'input': true, 'is-danger': $v.formFields.hearAboutUsDetail.$error, 'is-success': !$v.formFields.hearAboutUsDetail.$error && $v.formFields.hearAboutUsDetail.$dirty }"
+                       type="text"
+                       @input="$v.formFields.hearAboutUsDetail.$touch()"
+                       :placeholder="hearAboutUs.placeholder"
+                       v-model="formFields.hearAboutUsDetail">
               </p>
-              <p class="help is-danger">{{ hearAboutUs.error }}</p>
+              <p class="help is-danger" v-show="$v.formFields.hearAboutUsDetail.$error">{{ hearAboutUs.error }}</p>
             </div>
           </div>
         </div>
@@ -74,21 +94,32 @@
         <div class="field">
           <label class="label">Comentários</label>
           <p class="control">
-            <textarea :class="{ 'textarea': true, 'is-danger': true }" type="text" placeholder="Informe seu nome completo" v-model="formFields.description"></textarea>
+            <textarea :class="{ 'textarea': true, 'is-danger': $v.formFields.description.$error, 'is-success': !$v.formFields.description.$error && $v.formFields.description.$dirty }"
+                      type="text"
+                      @input="$v.formFields.description.$touch()"
+                      placeholder="Deixe aqui seus comentários"
+                      v-model="formFields.description"></textarea>
           </p>
+          <p class="help is-danger" v-show="$v.formFields.description.$error">Informe seus comentários.</p>
         </div>
       </div>
     </div>
     <div class="columns is=multiline">
       <div class="column">
-        <button class="button is-primary">Enviar mensagem</button>
+        <button class="button is-primary" @click="submitForm()">Enviar mensagem</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+Vue.use(Vuelidate)
+import { required, email, minLength } from 'vuelidate/lib/validators'
+
 import Switches from 'vue-switches'
+
 export default {
   data () {
     return {
@@ -117,7 +148,38 @@ export default {
       ]
     }
   },
+  validations: {
+    formFields: {
+      name: {
+        required
+      },
+      email: {
+        required,
+        email
+      },
+      phone: {
+        required,
+        minLength: minLength(10)
+      },
+      hearAboutUs: {
+        required
+      },
+      hearAboutUsDetail: {
+        required (v) {
+          console.log(v)
+          if (this.formFields.hearAboutUs !== 'Indicação' && this.formFields.hearAboutUs !== 'Outro') return true
+          if (v.length > 0) return true
+          return false
+        }
+      },
+      description: {
+      }
+    }
+  },
   methods: {
+    submitForm () {
+      this.$v.formFields.$touch()
+    },
     setHearAboutUsDetails () {
       this.hearAboutUs.label = 'Indicação *'
       this.hearAboutUs.placeholder = 'Informe a pessoa da indicação'
@@ -135,5 +197,15 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss">
+  @import "~bulma";
+
+  .is-danger-select {
+    border: 1px solid $danger !important;
+  }
+
+  .is-success-select {
+    border: 1px solid $success !important;
+  }
+
 </style>
